6e4f93c52f76a670956de65b71164f3d8618ff26
Bryan Bailey <bryan.bailey@bethesda.net>
2019-01-07 23:36:52 -0500

    index
    skeleton for index page

---
+++ bvje.py
@@ -0,0 +1,55 @@
+from flask import Flask, render_template, request, url_for
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+
+
+app = Flask(__name__)
+app.config['SECRET KEY'] = 'a cappella'
+bootstrap =  Bootstrap(app)
+moment = Moment(app)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+def calendar():
+    return render_template('calendar.html')
+
+
+@app.route('/members')
+def members():
+    return render_template('members.html')
+
+
+@app.route('/members/<username>')
+def user(username):
+    return render_template('members.html', username=username)
+
+
+def repertoire():
+    return render_template('repertoire.html')
+
+
+def gallery():
+    return render_template('gallery.html')
+
+
+def shop():
+    return render_template('shop.html')
+
+
+def contact():
+    return render_template('contact.html')
+
+
+def admin():
+    return render_template('admin.html')
+
+
+if __name__ == "__main__":
+    app.run()
