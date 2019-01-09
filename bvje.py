2e52b37fb7f766a028cc544fa75455225454d77d
Bryan Bailey <31677949+Rigil-Kent@users.noreply.github.com>
2019-01-08 22:28:48 -0500

    going home
    placeholder

--- bvje.py
+++ bvje.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, request, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SubmitField,SelectMultipleField 
+from wtforms.validators import DataRequired, Email, EqualTo
 
 
 app = Flask(__name__)
@@ -12,11 +12,40 @@ bootstrap =  Bootstrap(app)
 moment = Moment(app)
 
 
+class SignupForm(FlaskForm):
+    username = StringField('Username: ', validators=[DataRequired()])
+    email = StringField('Email: ', validators=[Email()])
+    passwd = PasswordField('Password: ', validators=[DataRequired()])
+    passrepeat = PasswordField('Repeat Password: ', validators=[EqualTo(passwd)])
+    voice = SelectMultipleField('Voice: ', choices=[('soprano', 'soprano'), ('alto', 'alto'), ('tenor', 'tenor'), ('baritone', 'baritone'), ('bass', 'bass')], validators=[DataRequired()])
+
+    submit = SubmitField('Register')
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name: ', validators=[DataRequired()])
+    email = StringField('Email: ', validators=[Email()])
+    phone = StringField('Phone: ', validators=[DataRequired()])
+    request = StringField('Song Request(s): ', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/about/director')
+def director():
+    return render_template('director.html')
+
+
+@app.route('/calendar')
 def calendar():
     return render_template('calendar.html')
 
@@ -36,6 +65,7 @@ def repertoire():
     return render_template('repertoire.html')
 
 
+@app.route('/gallery')
 def gallery():
     return render_template('gallery.html')
 
@@ -47,6 +77,11 @@ def shop():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 
 def admin():
